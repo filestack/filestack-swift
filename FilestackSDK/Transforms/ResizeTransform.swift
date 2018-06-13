8ef3bb48c8a5e74a8f40702c29d6d25dbd115fc5
Mihály Papp <michal@papp.pl>
2018-06-13 17:24:54 +0200

    ResizeTransform adjusted

--- FilestackSDK/Transforms/ResizeTransform.swift
+++ FilestackSDK/Transforms/ResizeTransform.swift
@@ -8,65 +8,51 @@
 
 import Foundation
 
-
 /**
-    Resizes the image to a given width and height using a particular fit and alignment mode.
+ Resizes the image to a given width and height using a particular fit and alignment mode.
  */
 @objc(FSResizeTransform) public class ResizeTransform: Transform {
-
-    /**
-        Initializes a `ResizeTransform` object.
-     */
-    public init() {
-
-        super.init(name: "resize")
-    }
-
-    /**
-        Adds the `width` option.
-
-        - Parameter value: The new width in pixels. Valid range: `1...10000`
-     */
-    @discardableResult public func width(_ value: Int) -> Self {
-
-        options.append((key: "width", value: value))
-
-        return self
-    }
-
-    /**
-        Adds the `width` option.
-
-        - Parameter value: The new width in pixels. Valid range: `1...10000`
-     */
-    @discardableResult public func height(_ value: Int) -> Self {
-
-        options.append((key: "height", value: value))
-
-        return self
-    }
-
-    /**
-        Adds the `fit` option.
-
-        - Parameter value: An `TransformFit` value.
-     */
-    @discardableResult public func fit(_ value: TransformFit) -> Self {
-
-        options.append((key: "fit", value: value))
-
-        return self
-    }
-
-    /**
-        Adds the `align` option.
-
-        - Parameter value: An `TransformAlign` value.
-     */
-    @discardableResult public func align(_ value: TransformAlign) -> Self {
-
-        options.append((key: "align", value: value))
-
-        return self
-    }
+  
+  /**
+   Initializes a `ResizeTransform` object.
+   */
+  public init() {
+    super.init(name: "resize")
+  }
+  
+  /**
+   Adds the `width` option.
+   
+   - Parameter value: The new width in pixels. Valid range: `1...10000`
+   */
+  @discardableResult public func width(_ value: Int) -> Self {
+    return appending((key: "width", value: value))
+  }
+  
+  /**
+   Adds the `width` option.
+   
+   - Parameter value: The new width in pixels. Valid range: `1...10000`
+   */
+  @discardableResult public func height(_ value: Int) -> Self {
+    return appending((key: "height", value: value))
+  }
+  
+  /**
+   Adds the `fit` option.
+   
+   - Parameter value: An `TransformFit` value.
+   */
+  @discardableResult public func fit(_ value: TransformFit) -> Self {
+    return appending((key: "fit", value: value))
+  }
+  
+  /**
+   Adds the `align` option.
+   
+   - Parameter value: An `TransformAlign` value.
+   */
+  @discardableResult public func align(_ value: TransformAlign) -> Self {
+    return appending((key: "align", value: value))
+  }
 }
