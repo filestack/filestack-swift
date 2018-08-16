e8defc4d679d4a019d482b8ca147ae517b25e848
Mihály Papp <michal@papp.pl>
2018-08-16 15:22:11 +0200

    refactor extensions

--- FilestackSDK/Extensions/UIColor+hexString.swift
+++ FilestackSDK/Extensions/UIColor+hexString.swift
@@ -8,22 +8,18 @@
 
 import UIKit
 
-
 extension UIColor {
-
-    internal var hexString: String {
-
-        var red: CGFloat = 0
-        var green: CGFloat = 0
-        var blue: CGFloat = 0
-        var alpha: CGFloat = 0
-
-        getRed(&red, green: &green, blue: &blue, alpha: &alpha)
-
-        return String(format: "%02X%02X%02X%02X",
-                      Int(round(red * 255)),
-                      Int(round(green * 255)),
-                      Int(round(blue * 255)),
-                      Int(round(alpha * 255)))
-    }
+  
+  var hexString: String {
+    var red: CGFloat = 0
+    var green: CGFloat = 0
+    var blue: CGFloat = 0
+    var alpha: CGFloat = 0
+    getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+    return String(format: "%02X%02X%02X%02X", colorInt(red), colorInt(green), colorInt(blue), colorInt(alpha))
+  }
+  
+  private func colorInt(_ cgFloat: CGFloat) -> Int {
+    return Int(round(cgFloat * 255))
+  }
 }
