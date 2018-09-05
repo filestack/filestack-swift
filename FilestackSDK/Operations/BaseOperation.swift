22390cb970425dcfdd2ad12ade13bc610f014f7d
Mihály Papp <michal@papp.pl>
2018-09-05 14:13:41 +0200

    set style to BaseOperation

--- FilestackSDK/Operations/BaseOperation.swift
+++ FilestackSDK/Operations/BaseOperation.swift
@@ -8,78 +8,65 @@
 
 import Foundation
 
-
-internal class BaseOperation: Operation {
-
-    // `isReady` property override boilerplate, as suggested by Apple
-    private var _ready: Bool = false
-
-    internal override var isReady: Bool {
-
-        get {
-            return _ready
-        }
-
-        set {
-            if _ready != newValue {
-                willChangeValue(forKey: "isReady")
-                _ready = newValue
-                didChangeValue(forKey: "isReady")
-            }
-        }
+class BaseOperation: Operation {
+  
+  // `isReady` property override boilerplate, as suggested by Apple
+  private var _ready: Bool = false
+  
+  override var isReady: Bool {
+    get {
+      return _ready
     }
-
-    // `isExecuting` property override boilerplate, as suggested by Apple
-    private var _executing: Bool = false
-
-    internal override var isExecuting: Bool {
-
-        get {
-            return _executing
-        }
-
-        set {
-            if _executing != newValue {
-                willChangeValue(forKey: "isExecuting")
-                _executing = newValue
-                didChangeValue(forKey: "isExecuting")
-            }
-        }
+    set {
+      if _ready == newValue { return }
+      willChangeValue(forKey: "isReady")
+      _ready = newValue
+      didChangeValue(forKey: "isReady")
     }
-
-    // `isFinished` property override boilerplate, as suggested by Apple
-    private var _finished: Bool = false
-
-    internal override var isFinished: Bool {
-
-        get {
-            return _finished
-        }
-
-        set {
-            if _finished != newValue {
-                willChangeValue(forKey: "isFinished")
-                _finished = newValue
-                didChangeValue(forKey: "isFinished")
-            }
-        }
+  }
+  
+  // `isExecuting` property override boilerplate, as suggested by Apple
+  private var _executing: Bool = false
+  
+  override var isExecuting: Bool {
+    get {
+      return _executing
     }
-
-    // `isCancelled` property override boilerplate, as suggested by Apple
-    private var _cancelled: Bool = false
-
-    internal override var isCancelled: Bool {
-
-        get {
-            return _cancelled
-        }
-
-        set {
-            if _cancelled != newValue {
-                willChangeValue(forKey: "isCancelled")
-                _cancelled = newValue
-                didChangeValue(forKey: "isCancelled")
-            }
-        }
+    set {
+      if _executing == newValue { return }
+      willChangeValue(forKey: "isExecuting")
+      _executing = newValue
+      didChangeValue(forKey: "isExecuting")
     }
+  }
+  
+  // `isFinished` property override boilerplate, as suggested by Apple
+  private var _finished: Bool = false
+  
+  override var isFinished: Bool {
+    get {
+      return _finished
+    }
+    set {
+      if _finished == newValue { return }
+      willChangeValue(forKey: "isFinished")
+      _finished = newValue
+      didChangeValue(forKey: "isFinished")
+    }
+  }
+  
+  // `isCancelled` property override boilerplate, as suggested by Apple
+  private var _cancelled: Bool = false
+  
+  override var isCancelled: Bool {
+    get {
+      return _cancelled
+    }
+    set {
+      if _cancelled == newValue { return }
+      willChangeValue(forKey: "isCancelled")
+      _cancelled = newValue
+      didChangeValue(forKey: "isCancelled")
+    }
+  }
 }
