3b05b035ea86bd673f0f31e0e973de67154f7a3a
Mihály Papp <michal@papp.pl>
2018-10-09 12:19:40 +0200

    split functions

--- FilestackSDK/Operations/MultipartUploadCommitOperation.swift
+++ FilestackSDK/Operations/MultipartUploadCommitOperation.swift
@@ -9,8 +9,7 @@
 import Foundation
 import Alamofire
 
-
-internal class MultipartUploadCommitOperation: BaseOperation {
+class MultipartUploadCommitOperation: BaseOperation {
 
     let apiKey: String
     let fileSize: UInt64
--- FilestackSDK/Operations/MultipartUploadCompleteOperation.swift
+++ FilestackSDK/Operations/MultipartUploadCompleteOperation.swift
@@ -9,7 +9,7 @@
 import Foundation
 import Alamofire
 
-internal class MultipartUploadCompleteOperation: BaseOperation {
+class MultipartUploadCompleteOperation: BaseOperation {
   
   let apiKey: String
   let fileName: String
@@ -51,49 +51,51 @@ internal class MultipartUploadCompleteOperation: BaseOperation {
   }
   
   override func main() {
-    
     guard !isCancelled else {
-      self.state = .finished
+      state = .finished
       return
     }
-    
-    self.state = .executing
-    
-    let url = URL(string: "multipart/complete", relativeTo: uploadService.baseURL)!
-    
-    let multipartFormData: (MultipartFormData) -> Void = { form in
-      form.append(self.apiKey.data(using: .utf8)!, withName: "apikey")
-      form.append(self.uri.data(using: .utf8)!, withName: "uri")
-      form.append(self.region.data(using: .utf8)!, withName: "region")
-      form.append(self.uploadID.data(using: .utf8)!, withName: "upload_id")
-      form.append(self.fileName.data(using: .utf8)!, withName: "filename")
-      form.append(String(self.fileSize).data(using: .utf8)!, withName: "size")
-      form.append(self.mimeType.data(using: .utf8)!, withName: "mimetype")
-      if let storeLocation = self.storeOptions.location.description.data(using: .utf8) {
-        form.append(storeLocation, withName: "store_location")
-      }
-      if let storeRegionData = self.storeOptions.region?.data(using: .utf8) {
-        form.append(storeRegionData, withName: "store_region")
-      }
-      if let storeContainerData = self.storeOptions.container?.data(using: .utf8) {
-        form.append(storeContainerData, withName: "store_container")
-      }
-      if let storePathData = self.storeOptions.path?.data(using: .utf8) {
-        form.append(storePathData, withName: "store_path")
-      }
-      if let storeAccessData = self.storeOptions.access?.description.data(using: .utf8) {
-        form.append(storeAccessData, withName: "store_access")
-      }
-      if self.useIntelligentIngestion {
-        form.append("true".data(using: .utf8)!, withName: "multipart")
-      } else {
-        form.append(self.parts.data(using: .utf8)!, withName: "parts")
-      }
-    }
-    
-    uploadService.upload(multipartFormData: multipartFormData, url: url) { response in
+    state = .executing
+    uploadService.upload(multipartFormData: multipartFormData, url: uploadUrl) { response in
       self.response = response
       self.state = .finished
     }
   }
+}
+
+private extension MultipartUploadCompleteOperation {
+  var uploadUrl: URL {
+    return URL(string: "multipart/complete", relativeTo: uploadService.baseURL)!
+  }
+  
+  func multipartFormData(form: MultipartFormData) {
+    form.append(apiKey.data(using: .utf8)!, withName: "apikey")
+    form.append(uri.data(using: .utf8)!, withName: "uri")
+    form.append(region.data(using: .utf8)!, withName: "region")
+    form.append(uploadID.data(using: .utf8)!, withName: "upload_id")
+    form.append(fileName.data(using: .utf8)!, withName: "filename")
+    form.append(String(fileSize).data(using: .utf8)!, withName: "size")
+    form.append(mimeType.data(using: .utf8)!, withName: "mimetype")
+    if let storeLocation = storeOptions.location.description.data(using: .utf8) {
+      form.append(storeLocation, withName: "store_location")
+    }
+    if let storeRegionData = storeOptions.region?.data(using: .utf8) {
+      form.append(storeRegionData, withName: "store_region")
+    }
+    if let storeContainerData = storeOptions.container?.data(using: .utf8) {
+      form.append(storeContainerData, withName: "store_container")
+    }
+    if let storePathData = storeOptions.path?.data(using: .utf8) {
+      form.append(storePathData, withName: "store_path")
+    }
+    if let storeAccessData = storeOptions.access?.description.data(using: .utf8) {
+      form.append(storeAccessData, withName: "store_access")
+    }
+    if self.useIntelligentIngestion {
+      form.append("true".data(using: .utf8)!, withName: "multipart")
+    } else {
+      form.append(parts.data(using: .utf8)!, withName: "parts")
+    }
+  }
+
 }
