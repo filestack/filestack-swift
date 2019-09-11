f81d4222eef48a4ef010cdad57043abf09d5d4c5
Ruben Nine <ruben@9labs.io>
2019-09-11 10:36:32 +0200

    Adding -x USE_SWIFT_RESPONSE_FILE=NO flags to jazzy for Xcode 11 (per https://github.com/realm/jazzy/issues/1087)

--- bin/generate-and-deploy-docs.sh
+++ bin/generate-and-deploy-docs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -o errexit #abort if any command fails
-jazzy
+jazzy -x USE_SWIFT_RESPONSE_FILE=NO # per https://github.com/realm/jazzy/issues/1087
 GIT_DEPLOY_DIR=docs GIT_DEPLOY_BRANCH=gh-pages GIT_DEPLOY_REPO=git@github.com:filestack/filestack-swift.git ./bin/deploy-docs.sh
