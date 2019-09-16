535a127493df86a36a3bf0f6d82fb7066e087a6c
Ruben Nine <ruben@9labs.io>
2019-09-16 18:29:08 +0200

    Jazzy 0.11 brings back support for Xcode 11, so this flag is no longer required.

--- bin/generate-and-deploy-docs.sh
+++ bin/generate-and-deploy-docs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -o errexit #abort if any command fails
-jazzy -x USE_SWIFT_RESPONSE_FILE=NO # per https://github.com/realm/jazzy/issues/1087
+jazzy
 GIT_DEPLOY_DIR=docs GIT_DEPLOY_BRANCH=gh-pages GIT_DEPLOY_REPO=git@github.com:filestack/filestack-swift.git ./bin/deploy-docs.sh
