2d1db649df5607e0617544114093eb97957bea8e
Ruben Nine <ruben@9labs.io>
2020-09-30 18:26:57 +0200

    Adding FilestackSDK.h

---
+++ Sources/FilestackSDK/FilestackSDK.h
@@ -0,0 +1,17 @@
+//
+//  FilestackSDK.h
+//  FilestackSDK
+//
+//  Created by Ruben Nine on 28/06/2017.
+//  Copyright © 2017 Filestack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FilestackSDK.
+FOUNDATION_EXPORT double FilestackSDKVersionNumber;
+
+//! Project version string for FilestackSDK.
+FOUNDATION_EXPORT const unsigned char FilestackSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FilestackSDK/PublicHeader.h>
