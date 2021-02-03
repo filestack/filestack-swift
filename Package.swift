f4d72a1938ec49b80031570fbae4797515c39d9c
Ruben Nine <ruben@9labs.io>
2021-02-03 11:09:03 +0100

    Revert last change, removing AlamoFire dependency from xcodeproj.

--- Package.swift
+++ Package.swift
@@ -12,11 +12,6 @@ let package = Package(
             type: .dynamic,
             targets: ["FilestackSDK"]
         ),
-        .library(
-            name: "ObjcDefs",
-            type: .dynamic,
-            targets: ["ObjcDefs"]
-        ),
     ],
     dependencies: [
         .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", .upToNextMajor(from: Version(9, 0, 0)))
