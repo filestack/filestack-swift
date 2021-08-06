88b52953d7d328b0dc69571da772f0920ec6ccde
Ruben Nine <ruben@9labs.io>
2021-08-06 16:21:33 +0200

    Using swift-doc for doc generation.

--- bin/generate-docs.sh
+++ bin/generate-docs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -o errexit #abort if any command fails
 
-jazzy --build-tool-arguments -Xswiftc,-sdk,-Xswiftc,$(xcrun --sdk iphonesimulator --show-sdk-path),-Xswiftc,-target,-Xswiftc,"x86_64-apple-ios11.0-simulator"
+swift doc generate Sources --module-name Filestack --format html --output docs --base-url https://filestack.github.io/filestack-ios/
