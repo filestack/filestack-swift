576340f37cc3b6738816c7ef57780ed574f8b9ae
Ruben Nine <ruben@9labs.io>
2021-08-06 16:24:55 +0200

    Updating base-url.

--- bin/generate-docs.sh
+++ bin/generate-docs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -o errexit #abort if any command fails
 
-swift doc generate Sources --module-name Filestack --format html --output docs --base-url https://filestack.github.io/filestack-ios/
+swift doc generate Sources --module-name Filestack --format html --output docs --base-url https://filestack.github.io/filestack-swift/
